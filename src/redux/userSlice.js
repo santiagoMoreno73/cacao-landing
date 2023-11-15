fa9c754bafdca981c0a8c57ecd2dc43dbbed05d4
santiagoMoreno73 <santimor2009@hotmail.com>
2023-11-15 00:07:26 -0500

    added redux store & slices

---
+++ src/redux/userSlice.js
@@ -0,0 +1,39 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const user =
+  localStorage.getItem("user") !== null
+    ? JSON.parse(localStorage.getItem("user"))
+    : {};
+
+const token =
+  localStorage.getItem("token") !== null ? localStorage.getItem("token") : "";
+
+const initialState = {
+  user: user,
+  token: token,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    addUser: (state, action) => {
+      const user = action.payload;
+      state.user.id = user.id;
+      state.user.name = user.name;
+      state.user.email = user.email;
+      state.user.address = user.address;
+      state.user.active = user.active;
+
+      localStorage.setItem("user", JSON.stringify(user));
+    },
+    addToken: (state, action) => {
+      const token = action.payload;
+
+      localStorage.setItem("token", token);
+    },
+  },
+});
+
+export const { addUser, addToken } = userSlice.actions;
+export default userSlice.reducer;

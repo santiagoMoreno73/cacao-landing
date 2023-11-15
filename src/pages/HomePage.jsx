4a5a057d9d18dc94c74b9d7275c7e163217392f6
santiagoMoreno73 <santimor2009@hotmail.com>
2023-11-15 00:06:45 -0500

    added homepage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,21 @@
+import Home from "./../Components/Home/Home";
+import Search from "./../Components/Search/Search";
+import Support from "./../Components/Support/Support";
+import Travelers from "./../Components/Travelers/Travelers";
+import Info from "./../Components/Info/Info";
+import Lounge from "./../Components/Lounge/Lounge";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Home />
+      <Search />
+      <Support />
+      <Info />
+      <Lounge />
+      <Travelers />
+    </div>
+  );
+};
+
+export default HomePage;
